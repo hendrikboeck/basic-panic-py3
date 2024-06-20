d3ea8163558ed6c2b21133b0d09551ea1da11a3c
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2024-06-20 19:16:16 +0200

    increase test coverage to 100%

--- tests/test_00_basic_panic/test_00_panic.py
+++ tests/test_00_basic_panic/test_00_panic.py
@@ -1,12 +1,29 @@
 import pytest
 
-from basic_panic import Panic, PanicHandler, panic
+from basic_panic import Panic, PanicHandler, panic, SystemPanicHandler
 
 
 def test_panic():
     with pytest.raises(Panic) as exc_info:
         panic("Something went wrong")
     assert str(exc_info.value) == "Something went wrong"
+    assert repr(exc_info.value) == "Panic(message='Something went wrong', code=1)"
+
+
+def test_panic_handler():
+
+    # -- TEST: if on_panic is not implemented in a subclass of PanicHandler
+
+    class CustomPanicHandler2(PanicHandler):
+
+        def on_panic(self, panic: Panic) -> None:
+            super().on_panic(panic)
+
+    with pytest.raises(NotImplementedError):
+        with CustomPanicHandler2():
+            panic("Something went wrong")
+
+    # -- TEST: if on_panic is implemented in a subclass of PanicHandler
 
     class CustomPanicHandler(PanicHandler):
 
@@ -18,4 +35,13 @@ def test_panic():
         with pytest.raises(Panic) as exc_info:
             panic("Something went wrong")
         assert str(exc_info.value) == "Something went wrong"
-    # Panic caught: Something went wrong
+
+
+def test_system_panic_handler(capfd):
+
+    with pytest.raises(SystemExit) as exc_info:
+        with SystemPanicHandler():
+            panic("Something went wrong")
+
+        assert exc_info.value.code == 1
+        assert capfd.readouterr().out == "PANIC [Errno 1]: Something went wrong\n"
